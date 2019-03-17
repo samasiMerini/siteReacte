51ac9fb65cf846ea18accb413f2080f0808cdf57
merini <merini.abde@gmail.com>
2019-03-17 09:49:37 +0100

    button 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 const Home = ()=> (
     <div> <h1>Chers Lecteurs Bonjours !</h1>
     <p>This is template for marketing pages</p>
-    <Button to="/no-offres" variant="primary">Nos Offres </Button>
+    <Button href="/no-offres" variant="primary">Nos Offres </Button>
     </div>
 )
 export default Home;
